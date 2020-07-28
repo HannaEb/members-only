5ad031903a331e8ed809d52945a1221b48ba33ae
Hanna <hannaeberhoefer@yahoo.com>
2020-07-28 14:28:02 +0100

    Add users feature tests

---
+++ spec/features/users_feature_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+feature 'User can sign in and out' do
+
+  context 'user not signed in' do
+    scenario 'should display a sign up link and a sign in link' do
+      visit('/')
+      expect(page).to have_link('Sign up')
+      expect(page).to have_link('Sign in')
+    end
+
+    scenario 'should not display a sign out link' do
+      visit('/')
+      expect(page).not_to have_link('Sign out')
+    end
+  end
+
+  context 'user signed in' do
+    before do
+      sign_up_user_one
+    end
+
+    scenario 'should display a sign out link' do
+      expect(page).to have_link('Sign out')
+    end
+
+    scenario 'should not display a sign up link and a sign in link' do
+      expect(page).not_to have_link('Sign up')
+      expect(page).not_to have_link('Sign in')
+    end
+
+  end
+
+end
--- spec/helpers/application_helper.rb
+++ spec/helpers/application_helper.rb
@@ -1,3 +1,12 @@
-module ApplicationHelper 
+module ApplicationHelper
+
+  def sign_up_user_one
+    visit('/')
+    click_link('Sign up')
+    fill_in('Email', with: 'marley@example.com')
+    fill_in('Password', with: 'password')
+    fill_in('Password confirmation', with: 'password')
+    click_button('Sign up')
+  end 
 
 end
